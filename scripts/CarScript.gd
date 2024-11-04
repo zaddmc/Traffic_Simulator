e7fbce909b0ebb8b21fd5384962ec9c6a6b08982
zaddmc <davidbk927@gmail.com>
2024-11-04 12:36:05 +0100

    hello

--- scripts/CarScript.gd
+++ scripts/CarScript.gd
@@ -1,6 +1,7 @@
 class_name Car
 extends PathFollow3D
 
+# Collective varibles all instances can acces
 const my_scene: PackedScene = preload("res://scripts/car.tscn")
 static var CARS = []
 static var ROAD_DICT = []
@@ -10,38 +11,63 @@ var starting_offset: float
 var max_speed: float
 var speed: float
 var current_road: Path3D
+var current_roads = []
+var cars_on_same_road = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.set_progress(starting_offset)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var wanted_space = 0.05
+	var wanted_space = 2
 	var own_progress = self.get_progress_ratio()
 	#current_road.get_child(0).get_baked_length()
-	
-	var closest_car = CARS[0] if CARS[0] != self else CARS[1] 
-	for car in CARS:
-		if car == self:
-			continue
+
+	for car in cars_on_same_road:
+		if car == self: continue
 
 		var prog_diff_loop = car.get_progress_ratio() - own_progress
 		if prog_diff_loop < 0:
 			prog_diff_loop = 1 + prog_diff_loop
-		
+
 		if prog_diff_loop < wanted_space:
-			closest_car = car
-			self.speed = closest_car.speed
 			break
 		else:
 			self.speed = self.max_speed
-			
-	self.set_progress(self.get_progress()+delta*speed)
 
-static func new_car(starting_offset:float = 0, max_speed:float = 0) -> Car:
+	if own_progress >= 0.99:
+		change_road(ROAD_DICT[current_road].pick_random())
+
+	self.set_progress(self.get_progress()+delta*speed)
+	return
+
+func change_road(new_road:Path3D):
+	# Empty previous roads
+	current_road.remove_child(self)
+	current_roads = []
+	
+	new_road.add_child(self)
+	current_road = new_road
+	current_roads.append(new_road)
+	current_roads.append_array(ROAD_DICT[new_road])
+	
+	for road in current_roads:
+		var road_children = road.get_children() 
+		cars_on_same_road.append_array(road_children)
+		for child in road_children:
+			if child == self: continue
+			if not child.cars_on_same_road.has(self):
+				child.cars_on_same_road.append(self)
+
+	return
+
+static func new_car(road:Path3D, starting_offset:float = 0, max_speed:float = 0) -> Car:
 	var new_car: Car = my_scene.instantiate()
+	road.add_child(new_car)
+	new_car.current_road = road
+	new_car.change_road(road)
+	
 	new_car.starting_offset = starting_offset
 	
 	if (max_speed == 0):
@@ -49,6 +75,8 @@ static func new_car(starting_offset:float = 0, max_speed:float = 0) -> Car:
 	else:
 		new_car.max_speed = max_speed
 	new_car.speed = new_car.max_speed
+	
+	new_car.loop = false
 	
 	CARS.append(new_car)
 	return new_car
--- scripts/road_baker.gd
+++ scripts/road_baker.gd
@@ -2,12 +2,18 @@ extends Node3D
 
 var road_dict = {}
 var roads = []
-var temp_car = preload("res://scripts/car.tscn").instantiate()
-var temp_car_2 = preload("res://scripts/car.tscn").instantiate() 
+@onready var temp_car = self.get_child(0) #preload("res://scripts/car.tscn").instantiate()
+@onready var temp_car_2 = self.get_child(1) #preload("res://scripts/car.tscn").instantiate() 
 
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
+	call_deferred("_on_all_loaded")
+
+func _on_all_loaded():
 	roads = recursive_road_finder(self)
+	self.remove_child(temp_car)
+	self.remove_child(temp_car_2)
 	for road in roads:
 		road.add_child(temp_car)
 		temp_car.set_progress_ratio(1)
@@ -15,18 +21,20 @@ func _ready() -> void:
 		var close_roads = []
 		for rod in roads:
 			rod.add_child(temp_car_2)
-			var space_between = (temp_car.global_position - temp_car_2.global_position).length()
-			rod.remove_child(temp_car_2)
-			if space_between <= 3:
+			temp_car_2.set_progress_ratio(0)
+			var space_between = (temp_car_2.get_child(0).global_position - temp_car.get_child(0).global_position).length()
+			print(str(temp_car_2.global_position) + str(temp_car.global_position))
+			if space_between <= 0.1:
 				close_roads.append(rod)
+			rod.remove_child(temp_car_2)
 
 		road_dict[road] = close_roads
 		road.remove_child(temp_car)
 
 	# Giving the result to the cars
-	#print(road_dict)
+	print(road_dict)
 	Car.set_baked_roads(road_dict)
-	
+
 	spawn_cars()
 	return
 
@@ -46,6 +54,5 @@ func recursive_road_finder(input):
 @export var car_spawn_count: int = 10
 func spawn_cars():
 	for i in car_spawn_count:
-		var car = Car.new_car(0, 10)
-		roads.pick_random().add_child(car)
+		Car.new_car(roads.pick_random(), 0, 10)
 	return
