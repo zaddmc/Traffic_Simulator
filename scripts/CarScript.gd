63db84fbc69597ab4c004154a76c001304170f19
SpookyDK <105819593+SpookyDK@users.noreply.github.com>
2024-11-05 12:05:59 +0100

    Update CarScript.gd

--- scripts/CarScript.gd
+++ scripts/CarScript.gd
@@ -7,54 +7,61 @@ static var CARS = []
 static var ROAD_DICT = []
 
 # Internal varibles for each car object
-var starting_offset: float
 var max_speed: float
 var speed: float
 var current_road: Path3D
 var current_roads = []
 var cars_on_same_road = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	self.set_progress(starting_offset)
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func update_car(delta: float) -> void:
-	var wanted_space = 2
-	var own_progress = self.get_progress_ratio()
-	#current_road.get_child(0).get_baked_length()
+	var wanted_space = 5
+	var current_road_length = current_road.get_curve().get_baked_length()
 
+	var closest_car = null
+	var space_between_best: float = 1000
 	for car in cars_on_same_road:
 		if car == self: continue
 
-		var prog_diff_loop = car.get_progress_ratio() - own_progress
-		if prog_diff_loop < 0:
-			prog_diff_loop = 1 + prog_diff_loop
-
-		if prog_diff_loop < wanted_space:
-			break
+		var space_between
+		if car.current_road == self.current_road:
+			space_between = car.get_progress() - self.get_progress()
 		else:
-			self.speed = self.max_speed
+			space_between = (car.get_progress() + current_road_length) - self.get_progress()
 
-	if own_progress >= 0.99:
+		if space_between < space_between_best:
+			space_between_best = space_between
+			closest_car = car
+
+
+	if space_between_best < wanted_space:
+		if self.speed > 1:
+			self.speed *= 0.9
+	else:
+		self.speed = self.max_speed
+		#print("im called")
+
+
+	if self.get_progress_ratio() >= 0.99:
 		change_road(ROAD_DICT[current_road].pick_random())
 
 	self.set_progress(self.get_progress()+delta*speed)
 	return
 
 func change_road(new_road:Path3D):
-	# Empty previous roads
 	self.reparent(new_road)
-	current_roads = []
 	self.set_progress_ratio(0)
 	current_road = new_road
+
+	cars_on_same_road = []
+	current_roads = []
 	current_roads.append(new_road)
 	current_roads.append_array(ROAD_DICT[new_road])
 	
 	for road in current_roads:
 		var road_children = road.get_children() 
 		cars_on_same_road.append_array(road_children)
-		
+
 	return
 
 static func new_car(road:Path3D, starting_offset:float = 0, max_speed:float = 0) -> Car:
@@ -63,7 +70,7 @@ static func new_car(road:Path3D, starting_offset:float = 0, max_speed:float = 0)
 	new_car.current_road = road
 	new_car.change_road(road)
 	
-	new_car.starting_offset = starting_offset
+	new_car.set_progress_ratio(starting_offset) 
 	
 	if (max_speed == 0):
 		new_car.max_speed = randf_range(5, 15)
