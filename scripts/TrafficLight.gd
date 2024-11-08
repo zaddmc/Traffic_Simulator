5be3fd769987129dbf7f59b54d1c49e553bb000e
SpookyDK <esben9541@gmail.com>
2024-11-08 11:59:28 +0100

    traffic Dic

--- scripts/TrafficLight.gd
+++ scripts/TrafficLight.gd
@@ -2,9 +2,8 @@ class_name TrafficLight
 extends Node3D
 
 var roads = [] #list of roads in crossing
-var roadselect = [] #bool of every road, if true let cars through
-var lightselect = 0 #selects which road to let through
-var iswaiting = false
+var light_dic = {}
+var current_light = 0 #selects which road to let through
 
 # Called when the node enters the scene tree for the first time.
 func start() -> void:
@@ -12,25 +11,21 @@ func start() -> void:
 	var timer = get_child(-1)
 	timer.timeout.connect(update_trafficlight)
 	roads = get_children()
-	roads.remove_at(0)
+	roads.remove_at(-1)
 	for n in roads.size():
-		roadselect.append(false)
-
-func wait(seconds: float) -> void:
-	await get_tree().create_timer(seconds).timeout
+		light_dic[n] = false
+	light_dic[roads[0]] = true
 
 func update_trafficlight():
-	print("timer")
-	if iswaiting == false:
-		iswaiting = true
-		for i in range(roadselect.size()): roadselect[i] = false 
-		wait(1)
-		roadselect[lightselect-1] = true
-		if lightselect < roads.size():
-			lightselect = lightselect+1
-		else:
-			lightselect = 0
-		wait(6)
-		iswaiting = false
-		return
-	return
+	print("switching")
+	if current_light < len(light_dic):
+		light_dic[roads[current_light]] = false
+		current_light += 1 
+		light_dic[roads[current_light]] = true
+	else:
+		light_dic[roads[current_light]] = false
+		current_light = 0 
+		light_dic[roads[current_light]] = true
+
+
+		
