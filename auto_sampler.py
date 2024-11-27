69c1566bad609056e500d5072105aeee9db202a0
zaddmc <117923377+zaddmc@users.noreply.github.com>
2024-11-27 22:42:43 +0100

    Update auto_sampler.py

--- auto_sampler.py
+++ auto_sampler.py
@@ -1,16 +1,11 @@
 import subprocess
 import threading
-#subprocess.run(["ls", "-a"])
-#subprocess.run(["ls", "-a"])
-
-
 
 def start_thead(percent):
     for i in range(10):
-        subprocess.run(["godot", "-q", "--path", "./", f"{i}", f"{percent:02}"])
+        subprocess.run(["godot", "--quiet", "--headless", "--path", "./", f"{i}", f"{percent:02}"])
         print(f"percent: {percent:02} finished run: {i}")
     return
-
 
 threads_as = []
 for percent in range(0,110,10):
@@ -18,4 +13,3 @@ for percent in range(0,110,10):
 
 for thread in threads_as:
     thread.start()
-
