0cf6ada9b68041bff13e1cd24d5fb6849dc802d8
zaddmc <davidbk927@gmail.com>
2024-11-27 12:42:53 +0100

    Updated combiner

--- csv_combiner.py
+++ csv_combiner.py
@@ -3,18 +3,20 @@ import csv
 def start():
     
     csv_file_count = int(input("How many csv files: "))
+    percent = input("What percentage: ")
 
     master_dict = {}
 
-    for percent in range(0,110,10):
-        total = 0
-        for idx in range(csv_file_count):
-            total += import_csv_file(f"save_game{idx}-{percent}.csv")
-        master_data[percent].append(total / csv_file_count)
-        
+    #for percent in range(0,110,10):
+    total = 0
+    for idx in range(csv_file_count):
+        total += import_csv_file(f"data/data{percent}/save_game{idx}.csv")
+    master_data[percent].append(total / csv_file_count)
     
-def export_csv_file(data):
-    with open("exported_csv.csv", "w") as file:
+    export_csv_file(master_dict, percent)
+    
+def export_csv_file(data, percent):
+    with open(f"data/data{percent}/exported_{percent}.csv", "w") as file:
         writer = csv.writer(file)
         for percent in range(0,110,10):
             writer.writerow([data[percent]])
