d27fc0ae01a4e430f645103e0143ec0c48f6ccfe
zaddmc <davidbk927@gmail.com>
2024-12-04 10:45:06 +0100

    Small changes

--- scripts/MainControl.gd
+++ scripts/MainControl.gd
@@ -16,11 +16,12 @@ func _ready() -> void:
 
 func _on_all_loaded():
 	var roads = get_node("road_paths") # Gets road_baker.gd script
-	roads.bake_roads()
-	roads.assign_traffic_lights(light_time, lights_on, scale_int)
 	var args = Array(OS.get_cmdline_args())
 	var percent_given = float(float(args[1])/100.0)
-	print(percent_given)
+	print("The given percent of fast cars is: %s" % percent_given)
+
+	roads.bake_roads()
+	roads.assign_traffic_lights(light_time, lights_on, scale_int)
 	roads.spawn_cars(car_spawn_count, wanted_space, velocity_debug, spacing_multiplier, percent_given, scale_int)
 	return
 
